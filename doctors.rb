74d486aeccba20d9c94399f0097253f14345bd41
Justin Thibault <jvthibault@gmail.com>
2018-10-02 15:33:03 -0400

    added docs

--- doctors.rb
+++ doctors.rb
@@ -2,13 +2,16 @@ require 'rubygems'
 require 'csv'
 
 
-
-title_regex = /^(Director|Chief|Chair)/
+title_regex = /^(Director|Chief|Chair)[^Nurse]/
 file = "/Users/z/Downloads/doctors.csv"
 
 doctors = []
 file = File.open(file,"r:ISO-8859-1")
-csv = CSV.parse(file)
 
-titles = csv.map{|row| row[4]}.uniq
-emails =  csv.select{|row|  title_regex.match(row[3])  }.map{|row| row[4]}.uniq
+
+full_list = CSV.parse(file)
+only_matches =  csv.select{|row|  title_regex.match(row[3])  }
+
+
+titles = csv.map{|row| row[2] }.uniq.sort
+puts titles
